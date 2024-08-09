f1e4997739cb7d47498e8211a5232ba5fee4ef6c
shiva_kumar_vardhaman <100309373+shivakumarvardhamana@users.noreply.github.com>
2024-08-09 09:59:07 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,32 +1,3 @@
-# # from django.urls import path,include
-# # from .views import blog_api,likes_api,user_api,user_owner
-
-# # from rest_framework.routers import DefaultRouter
-# # router=DefaultRouter()
-# # import rest_framework
-# # # router.register('api',blog_api,basename="api")
-# # # router.register('api1',likes_api,basename="api1")
-# # # router.register('api2',user_api,basename="api2")
-# # router.register('api4',user_owner,basename="api4")
-# # urlpatterns=[
-# #     path('',include(router.urls)),
-# #     path('api',include('rest_framework.urls')),
-
-# # ]
-
-# # urls.py
-# from django.urls import path
-# # from . import views
-# from .views import KeyValueStoreView, SearchView
-
-# urlpatterns = [
-#     # path('get/', views.EmployeeList.as_view(), name='employee-list'),
-#     # path('create/', views.EmployeeCreate.as_view(), name='employee-create'),
-#     path("get/<str:key>", KeyValueStoreView.as_view()),
-#     path("set", KeyValueStoreView.as_view()),
-#     path("search", SearchView.as_view()),
-# ]
-
 from django.urls import path
 from .views import *
 
